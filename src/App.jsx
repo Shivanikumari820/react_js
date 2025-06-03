8c02d40573d05967e9c4cef39c9bab4748621589
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-06-03 17:54:15 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,16 @@
-// const App = () => {
-//   // function Display(){
-//   //   alert("Welcome to my website")
-//   // }
+import { useState } from "react";
 
-//   // const myname = (nm) =>{
-//   //   alert("Welcome " + nm + " to my website")
+const App=()=>{
+const [color,setColor]= useState("blue");
+return(
+<>
+<h2>Welcome to world</h2>
+<button onClick={()=>{setColor("yellow")}}>yellow</button>
+<button onClick={()=>{setColor("blue")}}>blue</button>
+<button onClick={()=>{setColor("green")}}>green </button>
+<h3 style={{color:color}}>my favourite colour :{color}</h3>
+</>
+)
 
-//   // }
-
-//   // const Display = (e) =>{
-//   //    console.log(e.type)
-//   // }
-
-//   // const myname = (nm) =>{
-//   //   alert(`MyName : ${nm} Type : ${e.type}`)
-
-//   // }
-
-//   //name , value , type jo lgana h event hamne vo alert msg hojaye print 
-//    function Display(e){
-//     let nm = e.target.name;
-//     let typ = e.target.type;
-//     let val = e.target.value;
-//     alert(`Name : ${nm}   Type : ${typ}    Value: ${val}`)
-
-
-//   }
-
-
-//   return (
-//    <>
-//    <h1>Welcome to App</h1>
-
-//    {/* <button onClick={Display}> Click here</button>                        callings a function in react js  */}
-
-//     {/* <button onClick={(e) => {myname("Shivani", e)}}> Click here</button>       kisi function k andr parameter pass krna ho to usko ek arrow function bnakr pass kre h ek onClick k and direct ni krte */}
-
-//     <button name="MyBtn1" type="Bnttyp" value="BTNVal" onClick={Display}> Click here</button>
-//    </>
-//   );
-// };
-
-// export default App;
-
-
-
-//input field me jo chnage krenge vo show hoga console me just value k baad 
-
-
-const App  = ()  => {
-
-  const handleInput = (e) => {
-    let name = e.target.name;
-    let value = e.target.value;
-    console.log([name], value)
-
-  }
-  return(
-    <>
-             Enter city : <input type="text" name = "MyCity" value = "Bhopal" onChange={handleInput} />
-    </>
-  );
 }
-
 export default App;
