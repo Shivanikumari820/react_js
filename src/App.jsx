8f46c3f579ba581c6b529b3acb30c2b2e5cf878d
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-06-03 18:08:48 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-const App=()=>{
-const [color,setColor]= useState("blue");
-return(
-<>
-<h2>Welcome to world</h2>
-<button onClick={()=>{setColor("yellow")}}>yellow</button>
-<button onClick={()=>{setColor("blue")}}>blue</button>
-<button onClick={()=>{setColor("green")}}>green </button>
-<h3 style={{color:color}}>my favourite colour :{color}</h3>
-</>
-)
+const App = () => {
+  const [count, setCount] = useState(0);
 
-}
+  const myInc = () => {
+    setCount(count + 1);
+  };
+
+  const myDecre = () => {
+    if (count === 0) {
+      alert("No Decrement");
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Counter App:</h1>
+      <button onClick={myInc}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={myDecre}>Decrement</button>
+    </>
+  );
+};
+
 export default App;
