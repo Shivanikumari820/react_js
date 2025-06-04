8232050d34ee0c21411c13628135bdb58e37c137
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-06-04 18:25:19 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [multi, setMulti] = useState(0);
+  // ek hi usestate bnakr sbka kaam hojaye sbke liye alg alg na bnana pde , ek hi input sbke liye kaam
+  const [formData, setFormData] = useState({rollno: "", name: "", city: "", fees: ""  });
 
+  const handleChange = (e) => {
+    // const { name, value } = e.target;
+    let name = e.target.name;          {/* name me city, ya rollno. aesa uska title */}
+    let value = e.target.value;        {/* value me Delhi, ya 121 aesa uski value aaye */}
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value             {/* [name] means key */}
+    }));
+  };
 
-  useEffect(() => {                  
-      setMulti(count * 2);              {/* ye useEffect dependency change hone pr chlegi 3rd type of useEffect h ye  */}
-  },[count]);                           {/* useEffect's second argument ([count]) is called the dependency array. It tells React: "Only run this effect when count changes." */}
+  const handleSubmit = () => {
+    console.log(formData);            {/* for print in console , concole me data dikhega */}
+  };
 
   return (
     <>
-      <h1>Welcome to my App</h1>    
-      <button onClick={() => {setCount(count+1)}}>Click here</button>
-    <h3>count is:   {count}</h3>
-    <h4>Multiplication : {multi}</h4>
+      <h1>Application Form</h1>
+
+      Enter Rollno: 
+      <input type="text"  name="rollno" value={formData.rollno} onChange={handleChange}/>
+      <br />
+
+      Enter Name: 
+      <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      <br />
+
+      Enter City: 
+      <input type="text" name="city" value={formData.city} onChange={handleChange}/>
+      <br />
+
+      Enter Fees: 
+      <input type="text" name="fees" value={formData.fees}   onChange={handleChange} />
+      <br />
+
+      <button onClick={handleSubmit}>Click!</button>
     </>
   );
 };
 
 export default App;
-
-/*
-This useEffect is the 3rd type (runs only when count changes):
-
-Every time the value of count changes, this code inside useEffect runs.
-
-It updates multi to be count * 2. */
