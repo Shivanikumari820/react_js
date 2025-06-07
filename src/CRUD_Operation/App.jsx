7a32375e328fe8f152f598933d002f8018f13b5b
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-06-07 11:50:03 +0530

    done

---
+++ src/CRUD_Operation/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Insert from "./pages/insert";
+import Display from "./pages/Display";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="insert" element={<Insert />} />
+        <Route path="display" element={<Display />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
